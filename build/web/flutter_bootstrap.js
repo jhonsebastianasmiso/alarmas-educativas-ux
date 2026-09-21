aa327a23705dc0c9fb8bded198ce009256b3e961
Sebastian Agudelo <sebastamriel@gmail.com>
2026-09-20 20:55:46 -0500

    chore: update web build assets and service worker version

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1081983937" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "16567784" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
